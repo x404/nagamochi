57e0114afd101f19c577ebb77e4460da9aa49ec8
x404 <kronos2003@gmail.com>
2018-08-29 18:39:13 +0300

    Добавлено боковое меню callback

--- app/js/engine.js
+++ app/js/engine.js
@@ -16,6 +16,29 @@ $(document).ready(function(){
 	});
 
 
+	// inputs
+	$('.input-field').each(function(){
+		if ($(this).find('.form-control').val().length > 0) {
+			return $(this).addClass('is-charged');
+		}
+	});
+
+	$('body').on('focusin', '.input-field', function(e) {
+		return $(this).addClass('is-focused');
+	})
+	.on('focusout', '.input-field', function(e) {
+		$(this).removeClass('is-focused');
+		return $(this).removeClass('is-focused');
+	})
+	.on('change', '.input-field', function() {
+		$(this).removeClass('is-charged');
+		if ($(this).find('.form-control').val().length > 0) {
+			return $(this).addClass('is-charged');
+		}
+	});
+
+
+	$('select').selectize();
 
 	// // mobile-menu
 	// $('#navbar').each(function(){
@@ -41,4 +64,24 @@ $(document).ready(function(){
 	// 		};
 	// 	init();
 	// });	
+});
+
+
+
+// обратный звонок
+$(document).on('click', '[data-toggle="sidemodal"]', function(e){
+	e.stopPropagation();
+	let target = this.dataset.target,
+		title = this.dataset.title;
+	document.querySelector(target).classList.add('open');
+	document.querySelector('.body').classList.add('m-modal-open');
+	// if (title != undefined) document.querySelector(target + ' #place').value = title;
+});
+
+
+$(document).on('click', '.close-modal', function(e){
+	$(this).closest('.open').removeClass('open');
+	document.querySelector('.body').classList.remove('m-modal-open');
+	// document.querySelector('body').classList.remove('modal-open');
+	// document.querySelector('body').classList.remove('modal-open-my');
 });
