ff2fa88090c3f1cc93c581b1dd7be1891163520f
x404 <kronos2003@gmail.com>
2018-08-29 15:57:27 +0300

    добавлен slick слайдер на главной

--- app/js/engine.js
+++ app/js/engine.js
@@ -1,64 +1,44 @@
 $(document).ready(function(){
 
-	// карусель
-	$('#foo1').owlCarousel({
-		loop:false,
-		nav:true,
+	$('#news-carousel').slick({
 		dots: false,
-		items:3,
-		startPosition : 2,
-		stagePadding : 250,
-		navText: ["", ""],
-		onInitialized: function (event) {
-			refreshFirstLastVisible(event);
-		},
-		onChanged: function (event) {
-			refreshFirstLastVisible(event);
-		},
-		responsive:{
-			0:{
-				items:1,
-				stagePadding: 20
-			},
-			900:{
-				items:2,
-				stagePadding: 0
-			},
-			992:{
-				items:1
-			},
-			1250:{
-				items:2
-			},
-			1550:{
-				items:3
-			}
-		}
+		prevArrow: '<button type="button" class="slick-prev slick-arrow" aria-label="Назад"></button>',
+		nextArrow: '<button type="button" class="slick-next slick-arrow" aria-label="Вперед"></button>',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: false,
+		autoplaySpeed: 4000,
+		arrows : true,
+		appendArrows : $('.prevnext'),
+        centerMode: true,
+        centerPadding: '105px',
+        infinite : true
 	});
 
 
-	// mobile-menu
-	$('#navbar').each(function(){
-		var $this = $(this),
-			$link = $('.navbar-toggle'),
-			$close = $('.close-menu'),
 
-			init = function(){
-				$link.on('click', openMenu);
-				$close.on('click', closeMenu);
-			},
-			openMenu = function(e){
-				e.preventDefault();
-				h = $(document).height();
-				$('body').addClass('o-menu');
-				$('#navbar').height(h);
+	// // mobile-menu
+	// $('#navbar').each(function(){
+	// 	var $this = $(this),
+	// 		$link = $('.navbar-toggle'),
+	// 		$close = $('.close-menu'),
 
-			},
-			closeMenu = function(e){
-				e.preventDefault();
-				$('body').removeClass('o-menu');
-				$('#navbar').height('auto');
-			};
-		init();
-	});	
+	// 		init = function(){
+	// 			$link.on('click', openMenu);
+	// 			$close.on('click', closeMenu);
+	// 		},
+	// 		openMenu = function(e){
+	// 			e.preventDefault();
+	// 			h = $(document).height();
+	// 			$('body').addClass('o-menu');
+	// 			$('#navbar').height(h);
+
+	// 		},
+	// 		closeMenu = function(e){
+	// 			e.preventDefault();
+	// 			$('body').removeClass('o-menu');
+	// 			$('#navbar').height('auto');
+	// 		};
+	// 	init();
+	// });	
 });
